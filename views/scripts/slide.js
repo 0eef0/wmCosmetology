b6dbdd3827f4fb10afb991857fe6e335c6517022
lgrave264 <71471845+lgrave264@users.noreply.github.com>
2022-05-05 15:18:59 -0700

    finished css, logos will be made tomorrow, page will be done tomorrow

--- views/scripts/slide.js
+++ views/scripts/slide.js
@@ -12,15 +12,15 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let slideBox = document.getElementsByClassName("slideBox");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < slideBox.length; i++) {
+    slideBox[i].className = slideBox[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slideBox[slideIndex-1].className += " active";
 }
