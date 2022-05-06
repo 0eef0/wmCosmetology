21d5ae8a217e21e7a5040a885f70a46b8d017fe3
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2022-05-06 16:21:51 -0700

    image upload working for one image at a time

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,6 +5,30 @@ const express = require('express')
 const app = express.Router()
 const bcrypt = require('bcrypt')
 const passport = require('passport');
+const cloudinary = require('cloudinary').v2;
+const stream = require('stream');
+const { ensureAuthenticated } = require('../middleware/auth');
+require('dotenv').config();
+
+// Cloudinary account info
+const apiSecret = process.env.CLOUDINARY_SECRET;
+const cloudName = 'west-mec-coding';
+const apiKey = '416953374243466';
+
+cloudinary.config({
+    cloud_name: cloudName, // add your cloud_name
+    api_key: apiKey, // add your api_key
+    api_secret: apiSecret, // add your api_secret
+    secure: true
+});
+
+(async () => {
+    console.log((await cloudinary.api.resources({
+        type: 'upload',
+        prefix: 'test2' // add your folder
+    })).resources);
+})()
+//
 
 const { updateAdminCutsByID } = require('../controllers/adminController');
 
@@ -67,7 +91,7 @@ app.post('/login', async (req, res, next) => { //login
     })(req, res, next)
 })
 
-app.get('/current', async(req, res) => {
+app.get('/current', async (req, res) => {
     if (req.user === undefined) {
         // The user is not logged in
         res.json({});
@@ -84,10 +108,10 @@ app.post('/logout', (req, res, next) => {
 })
 
 app.get('/', async (req, res) => {
-  try {
-      const allUsers = await UserSchema.find({});
-      res.status(201).json({ allUsers });
-  } catch (error) { res.status(500).json({ msg: error }) }
+    try {
+        const allUsers = await UserSchema.find({});
+        res.status(201).json({ allUsers });
+    } catch (error) { res.status(500).json({ msg: error }) }
 })
 
 app.get('/:id', async (req, res) => {
@@ -100,10 +124,27 @@ app.get('/:id', async (req, res) => {
 })
 
 app.delete('/', async (req, res) => {
-  try {
-      await UserSchema.deleteMany({});
-      res.status(201).json({ success: true, msg: "all users deleted" });
-  } catch (error) { res.status(500).json({ msg: error }) }
+    try {
+        await UserSchema.deleteMany({});
+        res.status(201).json({ success: true, msg: "all users deleted" });
+    } catch (error) { res.status(500).json({ msg: error }) }
+})
+
+app.post("/newVisit", (req, res) => {
+    if (!req.files.images.length) {
+        const cloudinaryStream = cloudinary.uploader.upload_stream({
+            folder: req.body.name
+        });
+        stream.Readable.from(req.files.images.data).pipe(cloudinaryStream);
+    } else {
+        const cloudinaryStream = cloudinary.uploader.upload_stream({
+            folder: req.body.name
+        });
+        req.files.images.forEach(img => {
+            stream.Readable.from(img.data).pipe(cloudinaryStream);
+        })
+    }
+    res.redirect('/newVisit');
 })
 
 module.exports = app;
