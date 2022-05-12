1bad0885a3bdcc5924e00cdeb87b17ff8a42e56c
Barik-Boley <71157911+xBarikadex@users.noreply.github.com>
2022-05-11 20:17:40 -0700

    Got rid of redundant code
    Standardized the html in a function that gets called with the appointments passed in

--- views/scripts/admin/schedule.js
+++ views/scripts/admin/schedule.js
@@ -1,5 +1,7 @@
 const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
-var newAppts = [];
+
+let newAppts = [];
+let filteredAppts = [];
 
 let apptsDOM = document.getElementById('appts-container');
 let acc = document.getElementsByClassName('title-row');
@@ -9,15 +11,13 @@ let modalTitles = document.getElementsByClassName('modal-title');
 let modalsYes = document.getElementsByClassName('confirm-btn');
 let modalsNo = document.getElementsByClassName('deny-btn');
 let addNotes = document.getElementsByClassName('add-notes');
-
 let allFilter = document.getElementById('all-filter');
 let dayFilter = document.getElementById('day-filter');
 let weekFilter = document.getElementById('week-filter');
 let monthFilter = document.getElementById('month-filter');
 let dateFilter = document.getElementById('date-filter');
 
-let filteredAppts = [];
-
+// Makes times readable
 function readableTime(time) {
     time = time.split(':');
     time.push('AM');
@@ -39,192 +39,14 @@ function readableDate(date) {
     return newDate;
 }
 
-async function getAppts() {
-    let appts = await axios.get('/api/v1/appointments');
-    appts = appts.data.appointments;
-    appts = appts.sort((a, b) => (a.date > b.date) ? 1 : (a.date === b.date) ? ((a.time > b.time) ? 1 : -1) : -1)
+// Adds div using inputted appointments
+function html(appts, dayFilter) {
     let newAppts = [];
-
-    let datet = new Date()
-
-    dayFilter.addEventListener('click', function () {
-        allFilter.classList.remove('active-filter');
-        dayFilter.classList.add('active-filter');
-        weekFilter.classList.remove('active-filter');
-        monthFilter.classList.remove('active-filter');
-
-        newAppts = [];
-        filteredAppts = appts;
-        appts = appts.filter((appt) => (parseInt(appt.date.split('-')[2], 10) == (datet.getDate()))), "today";
-        if (!appts.length) {
-            newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments today</p></div>`);
-        } else {
-            for (let i = 0; i < appts.length; i++) {
-                newAppts.push(`
-                    <div class="appointment-card">
-                        <div class="col">
-                            <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
-        
-                            <p class="date">${readableDate(appts[i].date)} @ ${readableTime(appts[i].time)}</p>
-                            <h1 class="name">${appts[i].name}</h1>
-                        </div>
-                        <div class="col right">
-                            <p class="estimated-price">Estimated Price: $${appts[i].price}</p>
-        
-                            <button class="cancel-appointment btn btn-3">Cancel</button>
-                        </div>
-                    </div>
-                `);
-            }
-        }
-
-        apptsDOM.innerHTML = newAppts.join('');
-        appts = filteredAppts;
-
-    })
-
-    weekFilter.addEventListener('click', function () {
-        allFilter.classList.remove('active-filter');
-        dayFilter.classList.remove('active-filter');
-        weekFilter.classList.add('active-filter');
-        monthFilter.classList.remove('active-filter');
-
-        newAppts = [];
-        filteredAppts = appts;
-        appts = appts.filter((appt) => (parseInt(appt.date.split('-')[2], 10) == (datet.getDate()) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 1) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 2) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 3) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 4) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 5) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 6) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 7)));
-        if (!appts.length) {
-            newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments this week</p></div>`);
-        } else {
-            for (let i = 0; i < appts.length; i++) {
-                newAppts.push(`
-                <div class="appointment-card">
-                    <div class="col">
-                        <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
-    
-                        <p class="date">${readableDate(appts[i].date)} @ ${readableTime(appts[i].time)}</p>
-                        <h1 class="name">${appts[i].name}</h1>
-                    </div>
-                    <div class="col right">
-                        <p class="estimated-price">Estimated Price: $${appts[i].price}</p>
-    
-                        <button class="cancel-appointment btn btn-3">Cancel</button>
-                    </div>
-                </div>
-            `);
-            }
-        }
-        apptsDOM.innerHTML = newAppts.join('');
-        appts = filteredAppts;
-    })
-
-    monthFilter.addEventListener('click', function () {
-        allFilter.classList.remove('active-filter');
-        dayFilter.classList.remove('active-filter');
-        weekFilter.classList.remove('active-filter');
-        monthFilter.classList.add('active-filter');
-
-        newAppts = [];
-        filteredAppts = appts;
-        appts = appts.filter((appt) => (parseInt(appt.date.split('-')[1], 10) == (datet.getMonth() + 1)));
-        if (!appts.length) {
-            newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments this month</p></div>`);
-        } else {
-            for (let i = 0; i < appts.length; i++) {
-                newAppts.push(`
-                    <div class="appointment-card">
-                        <div class="col">
-                            <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
-            
-                            <p class="date">${readableDate(appts[i].date)} @ ${readableTime(appts[i].time)}</p>
-                            <h1 class="name">${appts[i].name}</h1>
-                        </div>
-                        <div class="col right">
-                            <p class="estimated-price">Estimated Price: $${appts[i].price}</p>
-            
-                            <button class="cancel-appointment btn btn-3">Cancel</button>
-                        </div>
-                    </div>
-                `);
-
-            }
-        }
-        apptsDOM.innerHTML = newAppts.join('');
-        appts = filteredAppts;
-    })
-
-    dateFilter.addEventListener('change', function () {
-        newAppts = [];
-        filteredAppts = appts;
-        appts = appts.filter((appt) => (appt.date) == (dateFilter.value));
-        console.log(appts.length)
-        if (!appts.length) {
-            newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are no appointments with this date</p></div>`);
-        } else {
-            for (let i = 0; i < appts.length; i++) {
-                newAppts.push(`
-                <div class="appointment-card">
-                    <div class="col">
-                        <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
-    
-                        <p class="date">${readableDate(appts[i].date)} @ ${readableTime(appts[i].time)}</p>
-                        <h1 class="name">${appts[i].name}</h1>
-                    </div>
-                    <div class="col right">
-                        <p class="estimated-price">Estimated Price: $${appts[i].price}</p>
-    
-                        <button class="cancel-appointment btn btn-3">Cancel</button>
-                    </div>
-                </div>
-            `);
-
-            }
-        }
-        apptsDOM.innerHTML = newAppts.join('');
-        appts = filteredAppts;
-    })
-
-    allFilter.addEventListener('click', function () {
-        allFilter.classList.add('active-filter');
-        dayFilter.classList.remove('active-filter');
-        weekFilter.classList.remove('active-filter');
-        monthFilter.classList.remove('active-filter');
-
-        newAppts = [];
-        filteredAppts = appts;
-
-        if (!appts.length) {
-            newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments</p></div>`);
-        } else {
-            for (let i = 0; i < appts.length; i++) {
-                newAppts.push(`
-                    <div class="appointment-card">
-                        <div class="col">
-                            <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
-        
-                            <p class="date">${readableDate(appts[i].date)} @ ${readableTime(appts[i].time)}</p>
-                            <h1 class="name">${appts[i].name}</h1>
-                        </div>
-                        <div class="col right">
-                            <p class="estimated-price">Estimated Price: $${appts[i].price}</p>
-        
-                            <button class="cancel-appointment btn btn-3">Cancel</button>
-                        </div>
-                    </div>
-                `);
-            }
-        }
-        apptsDOM.innerHTML = newAppts.join('');
-        appts = filteredAppts;
-    })
+    filteredAppts = appts;
 
     if (!appts.length) {
         newAppts = [];
-        newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments</p></div>`);
+        newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments${!dayFilter ? '' : ` ${dayFilter}`}</p></div>`);
     } else {
         for (let i = 0; i < appts.length; i++) {
             newAppts.push(`
@@ -266,6 +88,7 @@ async function getAppts() {
         }
     }
     apptsDOM.innerHTML = newAppts.join('');
+    appts = filteredAppts;
 
     // Cancel Button Logic
     for (let i = 0; i < appts.length; i++) {
@@ -286,8 +109,8 @@ async function getAppts() {
             })
 
             modalsNo[i].addEventListener('click', function () {
-                modals[i + 1].style.display = "none";
-                modals[i + 1].style.visibility = "hidden";
+                modals[i+1].style.display = "none";
+                modals[i+1].style.visibility = "hidden";
             })
         })
     }
@@ -295,11 +118,8 @@ async function getAppts() {
     // Accordion Logic
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener('click', function () {
-            /* Toggle between adding and removing the 'active' class,
-            to highlight the button that controls the panel */
             this.classList.toggle('accordionActive');
 
-            /* Toggle between hiding and showing the active panel */
             let panel = this.nextElementSibling;
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
@@ -310,6 +130,72 @@ async function getAppts() {
             }
         });
     }
+}
+
+async function getAppts() {
+    let appts = await axios.get('/api/v1/appointments');
+    appts = appts.data.appointments.sort((a, b) => (a.date > b.date) ? 1 : (a.date === b.date) ? ((a.time > b.time) ? 1 : -1) : -1);
+    let newDate = new Date()
+
+    // Today Filter Logic
+    dayFilter.addEventListener('click', function () {
+        allFilter.classList.remove('active-filter');
+        dayFilter.classList.add('active-filter');
+        weekFilter.classList.remove('active-filter');
+        monthFilter.classList.remove('active-filter');
+        dateFilter.value = '';
+
+        filteredAppts = appts.filter((appt) => (parseInt(appt.date.split('-')[2], 10) == (newDate.getDate()))), "today";
+        html(filteredAppts, 'today');
+    })
+
+    // This Week Filter Logic
+    weekFilter.addEventListener('click', function () {
+        allFilter.classList.remove('active-filter');
+        dayFilter.classList.remove('active-filter');
+        weekFilter.classList.add('active-filter');
+        monthFilter.classList.remove('active-filter');
+        dateFilter.value = '';
+
+        filteredAppts = appts.filter((appt) => (parseInt(appt.date.split('-')[2], 10) == (newDate.getDate()) || parseInt(appt.date.split('-')[2], 10) == (newDate.getDate() + 1) || parseInt(appt.date.split('-')[2], 10) == (newDate.getDate() + 2) || parseInt(appt.date.split('-')[2], 10) == (newDate.getDate() + 3) || parseInt(appt.date.split('-')[2], 10) == (newDate.getDate() + 4) || parseInt(appt.date.split('-')[2], 10) == (newDate.getDate() + 5) || parseInt(appt.date.split('-')[2], 10) == (newDate.getDate() + 6) || parseInt(appt.date.split('-')[2], 10) == (newDate.getDate() + 7)));
+        html(filteredAppts, 'this week');
+    })
+
+    // This Month Filter Logic
+    monthFilter.addEventListener('click', function () {
+        allFilter.classList.remove('active-filter');
+        dayFilter.classList.remove('active-filter');
+        weekFilter.classList.remove('active-filter');
+        monthFilter.classList.add('active-filter');
+        dateFilter.value = '';
+
+        filteredAppts = appts.filter((appt) => (parseInt(appt.date.split('-')[1], 10) == (newDate.getMonth() + 1)));
+        html(filteredAppts, 'this month');
+    })
+
+    // Input Date Logic
+    dateFilter.addEventListener('change', function () {
+        allFilter.classList.remove('active-filter');
+        dayFilter.classList.remove('active-filter');
+        weekFilter.classList.remove('active-filter');
+        monthFilter.classList.remove('active-filter');
+
+        filteredAppts = appts.filter((appt) => (appt.date) == (dateFilter.value));
+        html(filteredAppts, `on ${readableDate(dateFilter.value)}`);
+    })
+
+    // All Appointments Logic
+    allFilter.addEventListener('click', function () {
+        allFilter.classList.add('active-filter');
+        dayFilter.classList.remove('active-filter');
+        weekFilter.classList.remove('active-filter');
+        monthFilter.classList.remove('active-filter');
+        dateFilter.value = '';
+
+        html(appts);
+    })
+
+    html(appts);
 }
 getAppts();
 
