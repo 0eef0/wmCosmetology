f6aec19bf7734127eef8aa287bff710e7aca901f
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2022-05-12 16:46:13 -0700

    Made navbar active work

--- views/scripts/navbar.js
+++ views/scripts/navbar.js
@@ -1,7 +1,33 @@
 const linksDivDOM = document.querySelector('.links');
-const barsDOM = document.querySelector('.fa-bars')
+const barsDOM = document.querySelector('.fa-bars');
+const navLinks = document.getElementsByClassName('link');
+let navbarDOM = document.getElementsByClassName('navbar')[0];
+const scheduleDOM = document.getElementById('schedule');
 
 const toggleNav = () => {
     linksDivDOM.classList.toggle("closed");
     barsDOM.classList.toggle("open");
+}
+
+// Adds Active class to current page
+for (let i = 0; i < navLinks.length; i++) {
+    if (`/${navLinks[i].id}` == window.location.pathname) {
+        navLinks[i].classList.add('active');
+    }
+}
+
+// Adds active class based on where you're scrolled to
+if (window.location.pathname == "/") {
+    window.addEventListener('scroll', function () {
+        let scrollPos = window.scrollY;
+        let scheduleOffset = scheduleDOM.offsetTop - navbarDOM.clientHeight;
+
+        if (scrollPos >= scheduleOffset) { 
+            navLinks[0].classList.remove('active');
+            navLinks[1].classList.add('active');
+        } else { 
+            navLinks[0].classList.add('active');
+            navLinks[1].classList.remove('active');
+        }
+    })
 }
