7540a92800604e1e63ab2df3b66ba48bb2f9b799
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2022-05-11 18:14:54 -0700

    Fixed Luca's css

--- views/scripts/admin/schedule.js
+++ views/scripts/admin/schedule.js
@@ -10,11 +10,11 @@ let modalsYes = document.getElementsByClassName('confirm-btn');
 let modalsNo = document.getElementsByClassName('deny-btn');
 let addNotes = document.getElementsByClassName('add-notes');
 
+let allFilter = document.getElementById('all-filter');
 let dayFilter = document.getElementById('day-filter');
 let weekFilter = document.getElementById('week-filter');
 let monthFilter = document.getElementById('month-filter');
 let dateFilter = document.getElementById('date-filter');
-let allFilter = document.getElementById('all-filter');
 
 let filteredAppts = [];
 
@@ -47,29 +47,58 @@ async function getAppts() {
 
     let datet = new Date()
 
-    dayFilter.addEventListener('click', function() {
-        dayFilter.style.backgroundColor = "purple";
-        dayFilter.style.color = "white";
-        dayFilter.style.border = "purple 2px solid";
-        weekFilter.style.backgroundColor = "white";
-        weekFilter.style.color = "black";
-        weekFilter.style.border = "black 2px solid";
-        monthFilter.style.backgroundColor = "white";
-        monthFilter.style.color = "black";
-        monthFilter.style.border = "black 2px solid";
-        allFilter.style.backgroundColor = "white";
-        allFilter.style.color = "black";
-        allFilter.style.border = "black 2px solid";
+    dayFilter.addEventListener('click', function () {
+        allFilter.classList.remove('active-filter');
+        dayFilter.classList.add('active-filter');
+        weekFilter.classList.remove('active-filter');
+        monthFilter.classList.remove('active-filter');
 
         newAppts = [];
         filteredAppts = appts;
-        appts = appts.filter((appt) => (parseInt(appt.date.split('-')[2],10) == (datet.getDate()))),"today";
-        if(!appts.length){
+        appts = appts.filter((appt) => (parseInt(appt.date.split('-')[2], 10) == (datet.getDate()))), "today";
+        if (!appts.length) {
             newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blanktext">There are currently no appointments today</p></div>`);
+            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments today</p></div>`);
         } else {
             for (let i = 0; i < appts.length; i++) {
-            newAppts.push(`
+                newAppts.push(`
+                    <div class="appointment-card">
+                        <div class="col">
+                            <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
+        
+                            <p class="date">${readableDate(appts[i].date)} @ ${readableTime(appts[i].time)}</p>
+                            <h1 class="name">${appts[i].name}</h1>
+                        </div>
+                        <div class="col right">
+                            <p class="estimated-price">Estimated Price: $${appts[i].price}</p>
+        
+                            <button class="cancel-appointment btn btn-3">Cancel</button>
+                        </div>
+                    </div>
+                `);
+            }
+        }
+
+        apptsDOM.innerHTML = newAppts.join('');
+        appts = filteredAppts;
+
+    })
+
+    weekFilter.addEventListener('click', function () {
+        allFilter.classList.remove('active-filter');
+        dayFilter.classList.remove('active-filter');
+        weekFilter.classList.add('active-filter');
+        monthFilter.classList.remove('active-filter');
+
+        newAppts = [];
+        filteredAppts = appts;
+        appts = appts.filter((appt) => (parseInt(appt.date.split('-')[2], 10) == (datet.getDate()) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 1) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 2) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 3) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 4) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 5) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 6) || parseInt(appt.date.split('-')[2], 10) == (datet.getDate() + 7)));
+        if (!appts.length) {
+            newAppts = [];
+            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments this week</p></div>`);
+        } else {
+            for (let i = 0; i < appts.length; i++) {
+                newAppts.push(`
                 <div class="appointment-card">
                     <div class="col">
                         <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
@@ -86,74 +115,22 @@ async function getAppts() {
             `);
             }
         }
-        
-        apptsDOM.innerHTML = newAppts.join('');
-        appts = filteredAppts;
-        
-    })
-
-    weekFilter.addEventListener('click', function() {
-        dayFilter.style.backgroundColor = "white";
-        dayFilter.style.color = "black";
-        dayFilter.style.border = "black 2px solid";
-        weekFilter.style.backgroundColor = "purple";
-        weekFilter.style.color = "white";
-        weekFilter.style.border = "purple 2px solid";
-        monthFilter.style.backgroundColor = "white";
-        monthFilter.style.color = "black";
-        monthFilter.style.border = "black 2px solid";
-        allFilter.style.backgroundColor = "white";
-        allFilter.style.color = "black";
-        allFilter.style.border = "black 2px solid";
-
-        newAppts = [];
-        filteredAppts = appts;
-        appts = appts.filter((appt) => (parseInt(appt.date.split('-')[2],10) == (datet.getDate()) || parseInt(appt.date.split('-')[2],10) == (datet.getDate()+1) || parseInt(appt.date.split('-')[2],10) == (datet.getDate()+2) || parseInt(appt.date.split('-')[2],10) == (datet.getDate()+3) || parseInt(appt.date.split('-')[2],10) == (datet.getDate()+4) || parseInt(appt.date.split('-')[2],10) == (datet.getDate()+5) ||parseInt(appt.date.split('-')[2],10) == (datet.getDate()+6) || parseInt(appt.date.split('-')[2],10) == (datet.getDate()+7)));
-        if(!appts.length){
-            newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blanktext">There are currently no appointments this week</p></div>`);
-        } else {
-            for (let i = 0; i < appts.length; i++) {
-            newAppts.push(`
-                <div class="appointment-card">
-                    <div class="col">
-                        <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
-    
-                        <p class="date">${readableDate(appts[i].date)} @ ${readableTime(appts[i].time)}</p>
-                        <h1 class="name">${appts[i].name}</h1>
-                    </div>
-                    <div class="col right">
-                        <p class="estimated-price">Estimated Price: $${appts[i].price}</p>
-    
-                        <button class="cancel-appointment btn btn-3">Cancel</button>
-                    </div>
-                </div>
-            `);
-        }
-        }
         apptsDOM.innerHTML = newAppts.join('');
         appts = filteredAppts;
     })
-    
-    monthFilter.addEventListener('click', function() {
-        dayFilter.style.backgroundColor = "white";
-        dayFilter.style.color = "black";
-        dayFilter.style.border = "black 2px solid";
-        weekFilter.style.backgroundColor = "white";
-        weekFilter.style.color = "black";
-        weekFilter.style.border = "black 2px solid";
-        monthFilter.style.backgroundColor = "purple";
-        monthFilter.style.color = "white";
-        monthFilter.style.border = "purple 2px solid";
-        allFilter.style.backgroundColor = "white";
-        allFilter.style.color = "black";
-        allFilter.style.border = "black 2px solid";
+
+    monthFilter.addEventListener('click', function () {
+        allFilter.classList.remove('active-filter');
+        dayFilter.classList.remove('active-filter');
+        weekFilter.classList.remove('active-filter');
+        monthFilter.classList.add('active-filter');
+
         newAppts = [];
         filteredAppts = appts;
-        appts = appts.filter((appt) => (parseInt(appt.date.split('-')[1],10) == (datet.getMonth()+1)));
-        if(!appts.length){
+        appts = appts.filter((appt) => (parseInt(appt.date.split('-')[1], 10) == (datet.getMonth() + 1)));
+        if (!appts.length) {
             newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blanktext">There are currently no appointments this month</p></div>`);
+            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments this month</p></div>`);
         } else {
             for (let i = 0; i < appts.length; i++) {
                 newAppts.push(`
@@ -171,24 +148,24 @@ async function getAppts() {
                         </div>
                     </div>
                 `);
-                
+
             }
         }
         apptsDOM.innerHTML = newAppts.join('');
         appts = filteredAppts;
     })
 
-    dateFilter.addEventListener('change', function() {
+    dateFilter.addEventListener('change', function () {
         newAppts = [];
         filteredAppts = appts;
         appts = appts.filter((appt) => (appt.date) == (dateFilter.value));
         console.log(appts.length)
-        if(!appts.length){
+        if (!appts.length) {
             newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blanktext">There are no appointments with this date</p></div>`);
+            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are no appointments with this date</p></div>`);
         } else {
             for (let i = 0; i < appts.length; i++) {
-            newAppts.push(`
+                newAppts.push(`
                 <div class="appointment-card">
                     <div class="col">
                         <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
@@ -203,32 +180,25 @@ async function getAppts() {
                     </div>
                 </div>
             `);
-            
+
             }
         }
         apptsDOM.innerHTML = newAppts.join('');
         appts = filteredAppts;
     })
 
-    allFilter.addEventListener('click', function(){
-        dayFilter.style.backgroundColor = "white";
-        dayFilter.style.color = "black";
-        dayFilter.style.border = "black 2px solid";
-        weekFilter.style.backgroundColor = "white";
-        weekFilter.style.color = "black";
-        weekFilter.style.border = "black 2px solid";
-        monthFilter.style.backgroundColor = "white";
-        monthFilter.style.color = "black";
-        monthFilter.style.border = "black 2px solid";
-        allFilter.style.backgroundColor = "purple";
-        allFilter.style.color = "white";
-        allFilter.style.border = "purple 2px solid";
+    allFilter.addEventListener('click', function () {
+        allFilter.classList.add('active-filter');
+        dayFilter.classList.remove('active-filter');
+        weekFilter.classList.remove('active-filter');
+        monthFilter.classList.remove('active-filter');
+
         newAppts = [];
         filteredAppts = appts;
 
-        if(!appts.length){
+        if (!appts.length) {
             newAppts = [];
-            newAppts.push(`<div class="appointment-card"><p class="blanktext">There are currently no appointments</p></div>`);
+            newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments</p></div>`);
         } else {
             for (let i = 0; i < appts.length; i++) {
                 newAppts.push(`
@@ -252,53 +222,57 @@ async function getAppts() {
         appts = filteredAppts;
     })
 
-    console.log('Appointments', appts);
-    for (let i = 0; i < appts.length; i++) {
-        newAppts.push(`
-            <div class="modal-container-brk">
-                <div class="cancel-modal">
-                    <h1 class="modal-title">Warning!</h1>
-                    <p>Are you sure you want to cancel ${appts[i].name}'s appointment?</p>
-                    <div class="modal-btns">
-                        <button class="confirm-btn">Yes</button>
-                        <button class="deny-btn">No</button>
+    if (!appts.length) {
+        newAppts = [];
+        newAppts.push(`<div class="appointment-card"><p class="blank-text">There are currently no appointments</p></div>`);
+    } else {
+        for (let i = 0; i < appts.length; i++) {
+            newAppts.push(`
+                <div class="modal-container-brk">
+                    <div class="cancel-modal">
+                        <h1 class="modal-title">Warning!</h1>
+                        <p>Are you sure you want to cancel ${appts[i].name}'s appointment?</p>
+                        <div class="modal-btns">
+                            <button class="confirm-btn">Yes</button>
+                            <button class="deny-btn">No</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="appointment-card">
-                <div class="col">
-                    <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
-                    ${appts[i].notes != '' ? `
-                        <div class="accordion">
-                            <div class="title-row appt-card-title-row">
-                                <p class="more-notes-title">Additional Notes</p>
-                                <i class="fas fa-angle-down"></i>
+                <div class="appointment-card">
+                    <div class="col">
+                        <h1 class="service-names">${formatter.format(appts[i].services)}</h1>
+                        ${appts[i].notes != '' ? `
+                            <div class="accordion">
+                                <div class="title-row appt-card-title-row">
+                                    <p class="more-notes-title">Additional Notes</p>
+                                    <i class="fas fa-angle-down"></i>
+                                </div>
+                                <div class="panel">
+                                    <p class="add-notes">${appts[i].notes}</p>
+                                </div>
                             </div>
-                            <div class="panel">
-                                <p class="add-notes">${appts[i].notes}</p>
-                            </div>
-                        </div>
-                    ` : ''}
+                        ` : ''}
 
-                    <p class="date">${readableDate(appts[i].date)} @ ${readableTime(appts[i].time)}</p>
-                    <h1 class="name">${appts[i].name}</h1>
-                </div>
-                <div class="col right">
-                    <p class="estimated-price">Estimated Price: $${appts[i].price}</p>
+                        <p class="date">${readableDate(appts[i].date)} @ ${readableTime(appts[i].time)}</p>
+                        <h1 class="name">${appts[i].name}</h1>
+                    </div>
+                    <div class="col right">
+                        <p class="estimated-price">Estimated Price: $${appts[i].price}</p>
 
-                    <button class="cancel-appointment btn btn-3">Cancel</button>
+                        <button class="cancel-appointment btn btn-3">Cancel</button>
+                    </div>
                 </div>
-            </div>
-        `);
+            `);
+        }
     }
     apptsDOM.innerHTML = newAppts.join('');
 
     // Cancel Button Logic
     for (let i = 0; i < appts.length; i++) {
-        cancelBtns[i].addEventListener('click', async function() {
-            modals[i+1].style.display = "flex";
-            modals[i+1].style.visibility = "visible";
-            modalTitles[i+1].style.color = "red";
+        cancelBtns[i].addEventListener('click', async function () {
+            modals[i + 1].style.display = "flex";
+            modals[i + 1].style.visibility = "visible";
+            modalTitles[i + 1].style.color = "red";
 
             modalsYes[i].addEventListener('click', async function () {
                 try {
@@ -311,16 +285,16 @@ async function getAppts() {
                 }
             })
 
-            modalsNo[i].addEventListener('click', function() {
-                modals[i+1].style.display = "none";
-                modals[i+1].style.visibility = "hidden";
+            modalsNo[i].addEventListener('click', function () {
+                modals[i + 1].style.display = "none";
+                modals[i + 1].style.visibility = "hidden";
             })
         })
     }
 
     // Accordion Logic
     for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener('click', function() {
+        acc[i].addEventListener('click', function () {
             /* Toggle between adding and removing the 'active' class,
             to highlight the button that controls the panel */
             this.classList.toggle('accordionActive');
@@ -341,7 +315,7 @@ getAppts();
 
 // Closes the Modal
 for (let i = 0; i < modals.length; i++) {
-    modals[i].addEventListener('click', function() {
+    modals[i].addEventListener('click', function () {
         modals[i].style.display = "none";
     });
 }
