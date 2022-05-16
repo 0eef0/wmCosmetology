10da22c0653724c31161d6cfcf75d54e586ce46f
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2022-05-16 16:17:18 -0700

    removed unused api stuff

--- routes/visitRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getAllVisits, createVisit } = require('../controllers/visitController')
-
-//Controllers for Appointment
-router.route('/').get(getAllVisits).post(createVisit);
-
-module.exports = router;
