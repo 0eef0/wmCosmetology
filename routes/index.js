fad42738ab6beeac599285de6e2bcdaed58fd5c8
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2022-05-16 14:54:07 -0700

    Got login and logout working
    Made createUser page only accessible to admin accounts, made all other admin pages only accessible to created accounts, added logout functionality, and hid createUser button to non-admins.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const navigation = express.Router();
 const axios = require('axios');
 //authentication middleware
-const { ensureAuthenticated } = require('../middleware/auth.js');
+const { ensureAuthenticated, ensureAdminAuthenticated } = require('../middleware/auth.js');
 
 const hairDescriptions = require('../hair-descriptions.json');
 
@@ -20,13 +20,13 @@ navigation.get('/login', (req, res) => {
 })
 
 //admin schedule page
-navigation.get('/schedule', (req, res) => {
+navigation.get('/schedule', ensureAuthenticated, (req, res) => {
     res.render('pages/admin/schedule', { title: "Admin Schedule" })
 })
 
 /* ALL ADMIN PAGES HERE */
 // Accounts
-navigation.get('/accounts',/* ensureAuthenticated, */ async (req, res) => {
+navigation.get('/accounts', ensureAuthenticated, async (req, res) => {
     try {
         const { data: { allUsers } } = await axios.get('http://localhost:5000/api/v1/admins');
 
@@ -57,25 +57,20 @@ navigation.get('/accounts',/* ensureAuthenticated, */ async (req, res) => {
 })
 
 /* ALL ADMIN PAGES HERE */
-
-// Accounts
-// navigation.get('/accounts', /* ensureAuthenticated, */(req, res) => {
-//     res.render('pages/admin/accounts')
-// })
 // Create User
-navigation.get('/createUser', /* ensureAuthenticated, */(req, res) => {
+navigation.get('/createUser', ensureAdminAuthenticated,(req, res) => {
     res.render('pages/admin/createUser', { title: "Create User" })
 })
 // New Appointment
-navigation.get('/newAppointment', /* ensureAuthenticated, */(req, res) => {
+navigation.get('/newAppointment', ensureAuthenticated, (req, res) => {
     res.render('pages/admin/newAppointment', { title: "New Appointment" })
 })
 // New Visit
-navigation.get('/newVisit', /* ensureAuthenticated, */(req, res) => {
+navigation.get('/newVisit', ensureAuthenticated, (req, res) => {
     res.render('pages/admin/newVisit', { hairDescriptions: hairDescriptions, title: "New Visit" })
 })
 // Schedule
-navigation.get('/schedule', /* ensureAuthenticated, */(req, res) => {
+navigation.get('/schedule', ensureAuthenticated, (req, res) => {
     res.render('pages/admin/schedule', { title: "Admin Schedule" })
 })
 
